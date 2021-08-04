9a62c0c638490ae4bb0fa5921436c9f52875e2d5
alvin222 <alvinroidoma72@gmail.com>
2021-08-04 12:25:37 +0700

    updates

--- src/main/java/com/vin/tutorial/student/StudentController.java
+++ src/main/java/com/vin/tutorial/student/StudentController.java
@@ -3,27 +3,45 @@ package com.vin.tutorial.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
+import java.time.LocalDate;
 import java.util.List;
 
-@RestController
-@RequestMapping(path="api/v1/student")
+@RestController // Define an object as a controller
+@RequestMapping(path="api/v1/student") // Define an object as request mapping
 public class StudentController {
 
     private final StudentService studentService;
 
-    @Autowired
+    @Autowired // link an object with other object
     public StudentController(StudentService studentService){
         this.studentService = studentService;
     }
 
-    @GetMapping
+    @GetMapping // Create a get function on object
     public List<Student> getStudents(){
         return studentService.getStudents();
     }
 
-    @PostMapping
+    @PostMapping // Create a post function on object with json as an argument when post
     public void registerNewStudent(@RequestBody Student student){
         studentService.addNewStudent(student);
     }
 
+    @DeleteMapping(path = "{studentId}") // Create a delete function with path as an argument to this function
+    public void deleteStudent(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}") // Create a put function to update data on web , with path as an argument (StudentID) and parameter name and email
+    public void updateStudent(
+                @PathVariable("studentId") Long studentId,
+                @RequestParam(required = false) String name,
+                @RequestParam(required = false) String email,
+                @RequestParam(required = false) String dob
+            )
+    {
+        studentService.updateStudent(studentId,name,email,dob);
+
+    }
 }
--- src/main/java/com/vin/tutorial/student/StudentService.java
+++ src/main/java/com/vin/tutorial/student/StudentService.java
@@ -3,10 +3,10 @@ package com.vin.tutorial.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
-import java.time.Month;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -24,5 +24,35 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("Email Taken");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId){
+        if (!studentRepository.existsById(studentId)){
+            throw new IllegalStateException("Student with id " + studentId + " doesn't exists");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email, String dob){
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("Student with id " + studentId + " doesn't exists"));
+        if (name != null && name.length() > 0 && !student.getName().equals(name)){
+            student.setName(name);
+        }
+        if (email != null && email.length() > 0 && !student.getEmail().equals(email)){
+            student.setEmail(email);
+        }
+
+        LocalDate parseDob = LocalDate.parse(dob);
+
+        if (!student.getDob().equals(parseDob)) {
+            student.setDob(parseDob);
+        }
     }
 }
